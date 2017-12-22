2877f1cf3e858d5f79840765bbc3b52839bf54d0
theIOTman <luan.sys@gmail.com>
2017-12-22 18:06:47 +0700

    Chapter 5, page 132/690

--- chapter04/chap04BattleShip/src/chap04BattleShip/TestBattleShipDrive.java
+++ chapter04/chap04BattleShip/src/chap04BattleShip/TestBattleShipDrive.java
@@ -2,22 +2,52 @@ package chap04BattleShip;
 
 public class TestBattleShipDrive {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+//	public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+//		BattleShip dot = new BattleShip();
+//		
+//		int[] locations = {2, 3, 4};
+//		dot.setLocationCells(locations);
+//		
+//		String userGuess = "8";
+//		String result = dot.checkYourSelf(userGuess);
+//		String testResult = "failed";
+//		
+//		if (result.equals("hit")) {
+//			testResult = "passed";
+//		}
+//		
+//		System.out.println(testResult);
+//	}
+
+
+	//Sharpen your pencil
+
+	public static void main(String [] args)
+	{
 		BattleShip dot = new BattleShip();
 		
-		int[] locations = {2, 3, 4};
+		int numOfGuesses = 0;
+		int location = (int) (Math.random() * 5);
+		int[] locations = {location, location + 1, location + 2};
+		String result;
+		String userGuess = "1";
+				
 		dot.setLocationCells(locations);
 		
-		String userGuess = "1";
-		String result = dot.checkYourSelf(userGuess);
-		String testResult = "failed";
+		boolean isAlive = true;
 		
-		if (result.equals("hit")) {
-			testResult = "passed";
+		
+		while(dot.numOfHits != 3)
+		{
+			System.out.print("Please input: ");
+			
+			result = dot.checkYourSelf(userGuess);
+			numOfGuesses++;
 		}
-		
-		System.out.println(testResult);
-	}
+		System.out.println("Congrats! Num Of hit: " + numOfGuesses);
 
+	}
+	
 }
+
