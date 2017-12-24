439d8bd2f81a07e92c1d960edf5db639fcc36b49
pmluan <luan.sys@gmail.com>
2017-12-25 00:23:43 +0700

    Magnet done

---
+++ chapter04/chap04Magnet/src/chap04Magnet/MultiFor.java
@@ -0,0 +1,20 @@
+package chap04Magnet;
+
+public class MultiFor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+			for(int x = 0; x < 4; x++){
+				for(int y= 4; y>2; y--){
+				if(x == 1){
+					x++;
+				}
+		System.out.println(x + " " + y);
+			}			
+		}
+
+	}
+
+}
